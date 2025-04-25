4eca87a11a72227cebd9428703652205e37364af
almond <almondawwawa@gmail.com>
2025-04-25 16:55:47 +0300

    Add get_color function

--- utils/visuals.py
+++ utils/visuals.py
@@ -1,9 +1,22 @@
 import sys
+from colorama import Fore
 
 def clear_input_line():
     sys.stdout.write('\x1b[1A')
     sys.stdout.write('\x1b[2K')
     sys.stdout.flush()
 
-def set_color():
-    pass
+def get_color(color):
+    color = color.lower()
+    color_map = {
+        'black': Fore.BLACK,
+        'red': Fore.RED,
+        'green': Fore.GREEN,
+        'yellow': Fore.YELLOW,
+        'blue': Fore.BLUE,
+        'magenta': Fore.MAGENTA,
+        'cyan': Fore.CYAN,
+        'white': Fore.WHITE,
+        'reset': Fore.RESET
+    }
+    return color_map.get(color, Fore.RESET)
