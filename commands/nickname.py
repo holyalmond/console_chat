56729b3f0c516bb9c864d06b8307c828cd1805bc
almond <almondawwawa@gmail.com>
2025-05-02 16:51:03 +0300

    Implement "/nickname" command

---
+++ commands/nickname.py
@@ -0,0 +1,13 @@
+from commands.base import command
+
+@command(name="nickname", description="Set your nickname")
+def nick(server, client, *args):    
+    client.conn.send("Enter your nickname:".encode())
+    client.nickname = client.conn.recv(1024).decode().strip()
+
+    while client.nickname in server.used_nicknames:
+        client.conn.send("Nickname is already taken. Choose another one:".encode())
+        client.nickname = client.conn.recv(1024).decode().strip()
+
+    server.used_nicknames.add(client.nickname)
+    server.clients[client.conn] = {"nickname": client.nickname, "color": client.color}
