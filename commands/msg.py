47577524be8179b4bc6519cf861d5aed2d548f27
holyalmond <almondawwawa@gmail.com>
2025-05-08 22:50:39 +0300

    Format private message display

--- commands/msg.py
+++ commands/msg.py
@@ -1,4 +1,8 @@
 from commands.base import command
+from utils.visuals import get_color
+
+from datetime import datetime
+from colorama import Fore, Style
 
 @command(name="msg", description="Send private message with: /msg nickname message")
 def online(server, client, *args):
@@ -8,4 +12,9 @@ def online(server, client, *args):
     for user_sock, user_info in server.clients.items():
         if user_info.get("nickname") == nickname:
             receiver_sock = user_sock
-    client.broadcast(message, client.conn, receiver_sock)
+    timestamp = datetime.now().strftime("%H:%M")
+    nickname_color = get_color(server.clients[client.conn]["color"])
+    full_message = (f"{Fore.LIGHTBLACK_EX}[{timestamp}]{Style.RESET_ALL} "
+                    f"{nickname_color}{client.nickname}{Style.RESET_ALL}(private): {message}")
+    client.broadcast(full_message, receiver_sock=receiver_sock)
+    client.conn.send(full_message.encode())
